05e4ce40f3c4991760a78e233c7db7d8481f87e6
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-07 22:43:54 +0100

    Routing examples

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'hello my man';
 });
+
+Route::get('/home', function() {
+    return 'my home';
+});
+
+Route::get('/home/{name}', function ($name){
+    return 'hello ' . $name;
+});
