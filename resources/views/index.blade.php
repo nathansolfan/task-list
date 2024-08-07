eb722c404fcbb49aa088e2d25fe8d3bb6ab8257b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-07 23:22:08 +0100

    @isset to dinally display $variable if available

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,6 @@
-Hello im a template
-And the name is : {{ $name }}
+<div>Hello im a template</div>
+
+{{-- Using isset only display this div if $name is available --}}
+@isset()
+<div>And the name is : {{ $name }}</div>
+@endisset
