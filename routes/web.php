dd3d796a87b0a644eee1e7a2b5f5ea0ae3a5b8ad
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-07 22:57:43 +0100

    ->name() and then inside return ->route() is passed the name

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@ Route::get('/home', function() {
     return 'my home';
 });
 
+// use the ->name('....') to use it as routes name
 Route::get('/home/{name}', function ($name){
     return 'hello ' . $name;
+})->name('hello');
+
+// the ->name() is passed inside ->route()
+Route::get('', function () {
+    return redirect()->route('hello');
 });
