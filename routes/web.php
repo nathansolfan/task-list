8ca7ca37c5e7cdbce6455f0175019a60298e3fee
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-08 13:46:49 +0100

    create id route

--- routes/web.php
+++ routes/web.php
@@ -61,21 +61,32 @@ Route::get('/', function () use ($tasks) {
     return view('index', [
         'tasks' => $tasks
     ]);
-});
+})->name('tasks.index');
 
-// use the ->name('....') to use it as routes name
-Route::get('/hello', function() {
-    return 'my home';
-})->name('hello');
+Route::get('/{id}', function ($id) {
+    return 'One single task';
+})->name('tasks.show');
 
-// the ->name() is passed inside ->route()
-Route::get('hallo', function () {
-    return redirect()->route('hello');
-});
 
-Route::get('/home/{name}', function ($name){
-    return 'hello ' . $name;
-});
+
+
+
+
+
+
+// // use the ->name('....') to use it as routes name
+// Route::get('/hello', function() {
+//     return 'my home';
+// })->name('hello');
+
+// // the ->name() is passed inside ->route()
+// Route::get('hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/home/{name}', function ($name){
+//     return 'hello ' . $name;
+// });
 
 Route::fallback(function (){
     return 'still works empty page';
