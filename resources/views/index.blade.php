0f1a3dd8f7a2f5592a78219af466c23b39fbf4e1
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-08 14:37:29 +0100

    using links where {{ $tasks->title }} are the text and the links go to each different id. Using: [ 'id' => $task->id ] array

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,8 @@
 
      @forelse ( $tasks as $task )
       <div>
-         <a href="{{route('tasks.show'), ['id' => $task->id]}}"></a> {{$task->title}}
-        </div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{$task->title}}</a>
+    </div>
      @empty
 
     <div>
